bd13e6d7b5e2fc63b3a9613de35e6f5257817a7f
Edmundo Corona <71851384+edmundocorona@users.noreply.github.com>
2023-10-24 04:58:55 -0600

    refactor: readability

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
 const convertToCelsius = function(fahrenheit) {
-  return Math.ceil(((fahrenheit - 32) * 5 / 9), -1);
+  const celsius = (fahrenheit - 32) * 5 / 9;
+  const DECIMALS = 1;
+  return ceil10(celsius, DECIMALS);
 };
 
 const convertToFahrenheit = function(celsius) {
-  return Math.ceil((celsius * 9 / 5 + 32), -1);
+  const fahrenheit = celsius * 9 / 5 + 32;
+  const DECIMALS = 1;
+  return ceil10(fahrenheit, DECIMALS);
+};
+
+const ceil10 = function(num, decimals = 0){
+  return Math.ceil(num * 10 ** decimals) / 10 ** decimals;
 };
 
 // Do not edit below this line
