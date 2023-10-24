5bcc463945b3399ce9e7076bd98602aa1fca569a
Edmundo Corona <71851384+edmundocorona@users.noreply.github.com>
2023-10-24 03:24:35 -0600

    add function sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(floorInteger, cellInteger) {
+  let sum = 0;
+  for (floorInteger; floorInteger <= cellInteger; floorInteger++) {
+    sum += floorInteger;
+  }
+  return sum;
 };
 
 // Do not edit below this line
