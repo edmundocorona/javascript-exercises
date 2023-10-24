38d612441cea9c60d4a6cfc8d1b69faa7447b376
Edmundo Corona <71851384+edmundocorona@users.noreply.github.com>
2023-10-24 03:51:38 -0600

    fix edge cases
    - works with larger number first
    - returns ERROR with negative numbers
    - returns ERROR with non-number parameters
    - returns ERROR with non-number parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,15 @@
 const sumAll = function(floorInteger, cellInteger) {
+  if (typeof floorInteger != 'number') return 'ERROR';
+  if (typeof cellInteger != 'number') return 'ERROR';
+  if (floorInteger < 0 || cellInteger < 0) return 'ERROR';
   let sum = 0;
-  for (floorInteger; floorInteger <= cellInteger; floorInteger++) {
-    sum += floorInteger;
+  let i = floorInteger;
+  if (cellInteger < floorInteger) {
+    i = cellInteger;
+    cellInteger = floorInteger;
+  }
+  for (i; i <= cellInteger; i++) {
+    sum += i;
   }
   return sum;
 };
