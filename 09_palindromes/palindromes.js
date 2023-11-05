e5eee023a59076b55dd989b0de0db30f7c83a25a
Edmundo Corona <71851384+edmundocorona@users.noreply.github.com>
2023-11-05 04:06:25 -0600

    update: add palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const arrayString = string.toLowerCase().split('');
+  const alphanumerical = arrayString.filter(char => char.match(/\w/));
+  for (let i = 0; i < Math.floor(alphanumerical.length / 2); i++) {
+    if (alphanumerical[i] !== alphanumerical[alphanumerical.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+  
 };
 
 // Do not edit below this line
