8cf40a5ea968419fd1cafa2220d250b5a2243bfc
Edmundo Corona <71851384+edmundocorona@users.noreply.github.com>
2023-11-05 20:19:58 -0600

    update: add function findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  return array.reduce((oldest, current) => {
+    return calcAge(oldest) < calcAge(current) ? current : oldest;
+  });
 };
+
+const calcAge = function (person) {
+  let lastYear = person.yearOfDeath;
+  if (!lastYear) {
+    lastYear = new Date().getFullYear();
+  }
+  return lastYear - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
