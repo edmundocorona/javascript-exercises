154a9026d608814984afeb333727492c569bd719
Edmundo Corona <71851384+edmundocorona@users.noreply.github.com>
2023-11-05 18:42:26 -0600

    update: add fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n < 0) {
+    return "OOPS";
+  }
+  let last = 0;
+  let result = 1;
+  while (n > 1) {
+    const temp = result;
+    result += last;
+    last = temp;
+    n--;
+  }
+  return result;
 };
 
 // Do not edit below this line
